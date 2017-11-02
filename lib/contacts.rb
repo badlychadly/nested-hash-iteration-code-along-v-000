e0a924a72555e172716646e64129db3ed283ba5c
Chad Montoya <chadmontoya21@yahoo.com>
2017-11-02 02:58:23 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,21 +14,14 @@ require 'pry'
   #   }
   # }
 
-# def remove_strawberry(contacts)
-#   contacts.each do |person, contact_details_hash|
-#     contact_details_hash.each do |attribute, data|
-#       if attribute == :favorite_ice_cream_flavors
-#         data.delete_if {|ice_cream| ice_cream == "strawberry"}
-#       end
-#     end
-#   end
-# end
-
 def remove_strawberry(contacts)
-  contacts["Freddy Mercury"].each do |attribute, data|
-    if attribute == :favorite_ice_cream_flavors
-      data.delete_if {|ice_cream| ice_cream == "strawberry"}
-    binding.pry
+  contacts.each do |person, contact_details_hash|
+    contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+      end
     end
   end
 end
+
+
